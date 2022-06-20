9ba32b469d2e60740619eb77a0123ffa676b2c36
norphiz <103674674+norphiz@users.noreply.github.com>
2022-06-20 23:57:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,14 +15,25 @@
 #
 ###################################
 
+printf 'gtk-theme-name="Arc-Dark-solid"
+gtk-icon-theme-name="Arc"
+gtk-font-name="Cantarell 11"
+gtk-cursor-theme-name="Adwaita"
+gtk-cursor-theme-size=0
+gtk-toolbar-style=GTK_TOOLBAR_BOTH
+gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+gtk-button-images=1
+gtk-menu-images=1
+gtk-enable-event-sounds=1
+gtk-enable-input-feedback-sounds=1
+gtk-xft-antialias=1
+gtk-xft-hinting=1
+gtk-xft-hintstyle="hintfull"
+gtk-xft-rgba="rgb"' > /home/$(logname)/.gtkrc-2.0
+
 printf '#!/usr/bin/env bash
 
-redshift -P -O 3500K &
-~/.fehbg &
-slstatus &
-xrdb ~/.Xdefaults
-
-exec dwm' > /home/$(logname)/.xinitrc
+exec bspwm' > /home/$(logname)/.xinitrc
 
 printf '
 clear
@@ -53,8 +64,9 @@ Include = /etc/pacman.d/mirrorlist
 Server = https://universe.artixlinux.org/$arch
 Server = https://mirror1.artixlinux.org/universe/$arch
 Server = https://mirror.pascalpuffke.de/artix-universe/$arch
-Server = https://artixlinux.qontinuum.space:4443/artixlinux/universe/os/$arch
+Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
 Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+Server = https://ftp.crifo.org/artix-universe/
 
 #[lib32]
 #Include = /etc/pacman.d/mirrorlist
@@ -65,7 +77,7 @@ Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
 #[community]
 #Include = /etc/pacman.d/mirrorlist-arch' > /etc/pacman.conf
 
-pacman -Sy; pacman -S --noconfirm --needed gcc make pkgconf xorg-server xorg-xinit libxft ttf-roboto-mono ttf-font-awesome gtk-engines gtk-engine-murrine man-db redshift arc-solid-gtk-theme arc-icon-theme lxappearance xclip xf86-video-intel gnome-themes-extra
+pacman -Sy; pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xsetroot ttf-roboto-mono ttf-font-awesome gtk-engines gtk-engine-murrine man-db redshift arc-solid-gtk-theme arc-icon-theme xclip xf86-video-intel gnome-themes-extra
 
 printf 'Section "InputClass"
 	Identifier "My Mouse"
